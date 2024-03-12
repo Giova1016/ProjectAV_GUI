ebb8e737d7fad302d9d9d6fdada89bb7ec0eb612
Giova1016 <116507578+Giova1016@users.noreply.github.com>
2024-03-12 19:28:31 -0400

    Added main.py file
    Added the main.py file that contains the base code to render the qml GUI's

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQml import QQmlApplicationEngine
+
+
+app = QGuiApplication(sys.argv)
+
+engine = QQmlApplicationEngine()
+engine.quit.connect(app.quit)
+engine.load('Dark_Mode_GUI.qml')
+# engine.load('Light_Mode_GUI.qml')
+
+sys.exit(app.exec())
+
+# import sys
+# from PySide6 import __version__ as pyside_version
+
+# print(f"PySide6 version: {pyside_version}")
